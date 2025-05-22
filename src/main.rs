a546fe8be79f0203e5903ac4a464ab83951ce6be
Martin <riven@tae.moe>
2025-05-22 16:30:13 +0200

    start on main

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,24 @@
+#[cfg(not(target_os = "windows"))]
+compile_error!("This is very clearly a WINDOWS service smh my head");
+
 pub mod camera;
 pub mod util;
+
+pub use camera::*;
+pub use util::*;
+
+use std::ffi::OsStr;
+
+#[macro_use]
+extern crate windows_service;
+
+use windows_service::{
+    service::{ServiceControl, ServiceControlAccept, ServiceExitCode, ServiceState, ServiceStatus, ServiceType},
+    service_control_handler, service_dispatcher, Result as winres,
+};
+
+const SERVICE_NAME: &str = "shmemcam";
+const SERVICE_TYPE: ServiceType = ServiceType::OWN_PROCESS;
 
 fn main() {
     println!("Hello, world!");
