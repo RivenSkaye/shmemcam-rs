9ceebe64a931397174b6605270317ba9bf847565
Martin <riven@tae.moe>
2025-05-28 16:19:08 +0200

    push testing code

--- src/uninstaller.rs
+++ src/uninstaller.rs
@@ -1,5 +1,13 @@
+#![windows_subsystem = "windows"]
+
+use std::num::NonZeroUsize;
+
+use windows_ext::{ext::QWordExt, minwindef::*};
+use winmmf::{MMFLock, MemoryMappedFile, Mmf, Namespace, RWLock};
+
 fn main() -> windows_service::Result<()> {
     use std::{
+        io::Write,
         thread::sleep,
         time::{Duration, Instant},
     };
@@ -9,6 +17,38 @@ fn main() -> windows_service::Result<()> {
         service_manager::{ServiceManager, ServiceManagerAccess},
     };
     use windows_sys::Win32::Foundation::ERROR_SERVICE_DOES_NOT_EXIST;
+
+    fn hook(boop: &std::panic::PanicHookInfo) {
+        use std::io::Write;
+        let mut panicfile =
+            std::fs::File::create(std::env::current_exe().unwrap().with_file_name("shmemcam.panic.log")).unwrap();
+        {
+            writeln!(panicfile, "panicking harder than a nigger on steroids! {boop}").unwrap();
+        }
+    }
+
+    std::panic::set_hook(Box::new(hook));
+
+    let mut logfile =
+        std::fs::File::create(std::env::current_exe().unwrap().with_file_name("shmemcam.panic.log")).unwrap();
+    let bufflen = 1179648;
+    writeln!(logfile, "Set buffer length to {:?}", bufflen.split()).unwrap();
+    let mmf = MemoryMappedFile::<RWLock>::new(NonZeroUsize::new(bufflen).unwrap(), "pogchamp", Namespace::GLOBAL);
+    writeln!(logfile, "MMF opened?").unwrap();
+    if mmf.is_err() {
+        writeln!(logfile, "MMF errored out the ass: {}", mmf.unwrap_err()).unwrap();
+        panic!("death")
+    }
+    writeln!(logfile, "MMF opened!").unwrap();
+    let mmf = mmf.unwrap();
+    writeln!(logfile, "MMF unwrapped").unwrap();
+    mmf.write_spin(
+        "fgsadjfvdsvbsdlhfsdliufgewlkfglskfglksdvlliusdgsfuglbvlbv".as_bytes(),
+        None::<fn(&dyn MMFLock, _) -> _>,
+    )
+    .unwrap();
+    writeln!(logfile, "MMF written to!").unwrap();
+    return Ok(());
 
     let manager_access = ServiceManagerAccess::CONNECT;
     let service_manager = ServiceManager::local_computer(None::<&str>, manager_access)?;
